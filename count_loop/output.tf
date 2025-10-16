b1f3cc4d2156c5f6865e79e31341b469a831b413
prathyusha <prathyusha0612@gmail.com>
2025-10-16 21:59:57 +0530

    Add count_loop directory

---
+++ count_loop/output.tf
@@ -0,0 +1,7 @@
+/* output "instance_names" {
+  value = [for instance in aws_instance.terraform : instance.tags["Name"]]
+} */
+
+# output "instances_output" {
+#   value       = aws_instance.terraform
+# }
---
+++ count_loop/variables.tf
@@ -0,0 +1,13 @@
+variable "instances" {
+    default = [ "mongodb", "redis", "mysql", "rabbitmq" ]
+}
+
+variable "zone_id"{
+    default = "Z0994508312PR2YKTSFA1"
+}
+
+variable "domain_name"{
+    default = "prathyusha.fun"
+}
+
+
